f2b47196c1b1dc7dd989d26890d174f88091090c
yra <ymz.ncnk@outlook.com>
2025-10-25 15:30:37 +0100

    Correct comments

--- cmd.go
+++ cmd.go
@@ -1,7 +1,3 @@
-// Package core-go contains the definitions for both cmd-stream-go client and
-// server.
-//
-// It provides interfaces such as Cmd, Result, and Proxy.
 package core
 
 import (
---
+++ core.go
@@ -0,0 +1,5 @@
+// Package core contains the definitions for both cmd-stream-go client and
+// server.
+//
+// It provides interfaces such as Cmd, Result, and Proxy.
+package core
